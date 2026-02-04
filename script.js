c0e02af70615c2abf293b8eadb6deaaaa1e039bd
Kuruva Venu Madhav <kuruvavenu24@gmail.com>
2026-02-04 16:56:39 +0530

    House Building

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ const sections = document.querySelectorAll(".fade-in");
 window.addEventListener("scroll", () => {
   sections.forEach(section => {
     const pos = section.getBoundingClientRect().top;
-    const screenHeight = window.innerHeight;
-
-    if (pos < screenHeight - 100) {
+    if (pos < window.innerHeight - 100) {
       section.classList.add("show");
     }
   });
